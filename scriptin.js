0c14ca763b90341051c069baef2bcb625a260d92
Rolex <101451384+r0l3x007@users.noreply.github.com>
2023-09-08 18:35:26 +0300

    Added toggle button but needs more tweaking as it executes 2 times in some scenarios not changing the value'

--- scriptin.js
+++ scriptin.js
@@ -11,15 +11,11 @@ let cardCont =  document.querySelector(`#bookDisp`);
     let inPages;
     let inStatus;
 
-    let myLibrary=[/*{title: `human`,
-    author: `JJ Simons`,
-    pages: `169`,
-    status: `read` },
-    {title: `humaasdn`,
-    author: `JJ Siasdsadmons`,
-    pages: `16944`,
-status: `In progress` }*/]
+    let myLibrary=[]
 
+
+
+    //Submit button on click will add the book to the array while adding the buttons 
 sbmitBtn.addEventListener(`click`, function(event){
     event.preventDefault();
     for(let bookStat of bookStatus){
@@ -38,19 +34,25 @@ sbmitBtn.addEventListener(`click`, function(event){
                 deleteBook(index);
             });
         }
+        
     }
     }
+    let sttButton = [...document.getElementsByClassName(`tgBtn`)];
+   /* for(let s of sttButton ){
+        s.addEventListener(`click`,function(){
+            let index = this.id;
+            changeStatus(index);
+        })
+    }*/
+    sttButton.forEach(element => {
+        element.addEventListener(`click`,function(){
+            let index = this.id;
+            changeStatus(index);
+        })
+        
+    });
     
 });
-
-/*if(myLibrary.length > 0){
-
-    let dltBkbtn =  document.getElementsByTagName(`button`);
-    for(let b of dltBkbtn){
-        b.addEventListener(`click`,deleteBook);
-    }
-}*/
-
 
 
 
@@ -62,11 +64,12 @@ function Book(title, author,pages,status){
     this.status =  status;
 
 }
-
+//changing the default toString value so the information is displayed correctly
 Book.prototype.toString = function(){
     return `The title of the book is: ` + this.title + ` Written by: ` + this.author + ` Number of pages ` + this.pages + ` Current Status ` + this.status; 
 }
 
+//eventlisteners for user input
 bookTitle.addEventListener(`input`, function(){
     inTitle = bookTitle.value;
 } );
@@ -79,8 +82,7 @@ bookPages.addEventListener(`input`, function(){
     inPages =  bookPages.value;
 });
 
-
-
+//creates a new book objects add adds it to the array
 
 function addBookToLibrary(bookTitle,bookAuthor,bookPages,bookStatus){
 
@@ -95,24 +97,30 @@ let bookHldr1 = document.querySelector(`bookHolder1`);
 
 
 
-function checkstuff(){
-    if(number > 0){
-        return drawCard(number);
-    }
-}
-
+//Dynamically generates div`s and button`s for the books where they will be displayed, each div is assigned to the book object index.
 function dispBook(){
     //for(let i = 0; i < myLibrary.length; i++){
         let cardBox = document.createElement(`div`);
         let dltBtn = document.createElement(`button`);
+        let toggleBtn = document.createElement(`button`);
+        let txtHolder =  document.createElement(`div`);
+        txtHolder.classList.add(`textHolder`);
         cardBox.classList.add(`bookHolder`);
         let txtObj = myLibrary[myLibrary.length-1].toString();
-        cardBox.textContent = txtObj;
+        txtHolder.textContent = txtObj;
+        toggleBtn.textContent = `Change Status`;
         dltBtn.textContent =`Delete`;
         dltBtn.classList.add(`btn`);
+        toggleBtn.classList.add(`tgBtn`);
+        txtHolder.setAttribute(`id`,`txt${myLibrary.length-1}`)
         dltBtn.setAttribute(`id`,myLibrary.length-1);
+        toggleBtn.setAttribute(`id`,myLibrary.length-1);
+        toggleBtn.style.width = `50px`;
+        toggleBtn.style.height =`50px`;
         dltBtn.style.width = `50px`;
         dltBtn.style.height = `50px`;
+        cardBox.appendChild(txtHolder);
+        cardBox.appendChild(toggleBtn);
         cardBox.appendChild(dltBtn);
         cardBox.setAttribute(`id`,`box${myLibrary.length-1}`);
         cardBox.style.width =`200px`;
@@ -121,21 +129,31 @@ function dispBook(){
         cardCont.appendChild(cardBox);
 }
 
-/*if(myLibrary.length > 0){
 
-    let dltBkbtn =  document.getElementsByTagName(`button`);
-    for(let b of dltBkbtn){
-        b.addEventListener(`click`,deleteBook);
-    }
-}*/
-
+//Deletes the book based on the assigned index
 function deleteBook(index){
         //let bookLoc = parseInt(index);
         //myLibrary.splice(bookLoc,bookLoc);
         let dispcont = document.getElementById(`box${index}`);
         //cardCont.removeChild(dispcont);
+        if(dispcont != null ){
         dispcont.remove();
         delete myLibrary[index];
+        }
         
 }
 
+function changeStatus(index){
+    let statusUp = myLibrary[index].status;
+    let dispCont = document.getElementById(`txt${index}`);
+
+    if(statusUp == `Read`){
+        myLibrary[index].status = `Not read`;
+        dispCont.textContent = ` `;
+        dispCont.textContent = myLibrary[index].toString();
+    } else if(statusUp == `Not read`){
+        myLibrary[index].status = `Read`;
+        dispCont.textContent = ` `;
+        dispCont.textContent = myLibrary[index].toString();
+    }
+}
